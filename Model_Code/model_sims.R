8a958cb069578c9b39ae1c9a06b7934190792092
Stephanie Wu <stephwu18@boslogin01.rc.fas.harvard.edu>
2024-06-14 16:39:55 -0400

    Added scenario 11 data, and made changes (in progress) to run models without baysc package

--- Model_Code/model_sims.R
+++ Model_Code/model_sims.R
@@ -10,17 +10,41 @@ args <- commandArgs(trailingOnly = TRUE)
 scenario <- args[[1]]   # Simulation scenario
 samp_i <- args[[2]]     # Sample number
 
+# Whether or not baysc package has been installed. If FALSE, local functions 
+# will be used instead
+baysc_package = FALSE
+
 # Load libraries
-library(BART)
-library(baysc)
+library(BART)  # BART
 library(abind)  # binding arrays
 library(parallel)  # parallel processing
+library(plyr)
+library(dplyr)
+library(LaplacesDemon)  # distributions
+library(truncnorm)
+library(fastDummies)
+library(matrixStats)
+library(Matrix)
+library(gtools)
+library(e1071)
+library(rstan)  # stan
+library(survey)  # survey functions
+library(Rcpp)  # Rcpp interface
+library(RcppArmadillo)
+library(RcppTN)
 
 # Set directories
 wd <- "~/Documents/GitHub/WOLCAN/"  # Working directory
+wd <- "/n/holyscratch01/stephenson_lab/Users/stephwu18/WOLCAN/"
 data_dir <- "Data/"    # Data directory
 res_dir <- "Results/"  # Results directory
 code_dir <- "Model_Code/"  # Model code directory
+
+# Create scenario results folder if it doesn't exist
+dir_path <- paste0(wd, res_dir, "scen_", scenario, "/")
+if (!dir.exists(dir_path)) {
+  dir.create(file.path(dir_path))
+}
 
 # Define path to save results
 save_path <- paste0(wd, res_dir, "scen_", scenario, "/samp_", samp_i)
@@ -33,8 +57,26 @@ if (already_done) {
   
   print(paste0('Running scenario ', scenario, ' samp ', samp_i, '...'))
   
-  # Source functions
+  # Source R model functions
   source(paste0(wd, code_dir, "model_functions.R"))
+  
+  # Source baysc functions
+  if (baysc_package) {
+    library(baysc)
+    mod_stan <- NULL
+  } else {
+    # Source Rcpp functions from baysc package
+    Rcpp::sourceCpp(paste0(wd, code_dir, "baysc_functions/", "main_Rcpp_functions.cpp"))
+    
+    # Source functions from baysc package
+    file_list <- c("mcmc_functions_wolca.R", "mcmc_functions.R", "utilities.R", 
+                   "wolca_var_adjust.R", "wolca.R", "wolca_svyglm.R")
+    invisible(lapply(file_list, function(x) 
+      source(paste0(wd, code_dir, "baysc_functions/", x))))
+    
+    # Source stan models
+    mod_stan <- stan_model(paste0(wd, code_dir, "baysc_functions/", "WOLCA_main.stan"))
+  }
   
   ### Read in population and sample data
   load(paste0(wd, data_dir, "scen_", scenario, "/sim_pop_wolcan.RData"))
@@ -60,6 +102,7 @@ if (already_done) {
   # Model estimation
   D <- 10            # Number of sets of MI pseudo-weights
   parallel <- TRUE   # Whether to parallelize for MI
+  n_cores <- 4       # Number of cores to use for parallelization
   MI <- TRUE         # Whether to run MI procedure for variance estimation
   adjust <- TRUE     # Whether to adjust variance for pseudo-likelihood
   tol <- 1e-8        # Underflow tolerance
@@ -67,16 +110,16 @@ if (already_done) {
   K_max <- 30          # Max number of latent classes for adaptive sampler
   adapt_seed <- samp_i # Seed for adaptive sampler
   fixed_seed <- samp_i # Seed for fixed sampler
-  n_runs <- 10000    # Number of MCMC iterations
-  burn <- 5000      # Burn-in
+  n_runs <- 20000    # Number of MCMC iterations
+  burn <- 10000      # Burn-in
   thin <- 5          # Thinning
-  update <- 1000     # Display update frequency
+  update <- 5000     # Display update frequency
   
-  # # For tests
-  # n_runs <- 1000
-  # burn <- 500
-  # thin <- 5
-  # update <- 500
+  # For tests
+  n_runs <- 1000
+  burn <- 500
+  thin <- 5
+  update <- 500
   
   ### Modifications based on scenario
   if (scenario == 2) {  # No propagation of weights uncertainty
@@ -93,16 +136,17 @@ if (already_done) {
   res <- wolcan(x_mat = x_mat, dat_B = dat_B, dat_R = dat_R, 
                 pred_covs_B = pred_covs_B, pred_covs_R = pred_covs_R, 
                 pi_R = pi_R, hat_pi_R = hat_pi_R, num_post = num_post, 
-                frame_B = frame_B, frame_R = frame_R, D = D, parallel = parallel, 
+                frame_B = frame_B, frame_R = frame_R, D = D, 
+                parallel = parallel, n_cores = n_cores, 
                 MI = MI, adjust = adjust, tol = tol, run_adapt = run_adapt, 
                 K_max = K_max, adapt_seed = adapt_seed, K_fixed = NULL, 
                 fixed_seed = fixed_seed, class_cutoff = 0.05, 
                 n_runs = n_runs, burn = burn, thin = thin, update = update, 
-                save_res = TRUE, save_path = save_path)
+                save_res = TRUE, save_path = save_path, mod_stan = mod_stan)
   
   ### Run unweighted model
   if (scenario %in% c(1, 7:11)) {
-    res_unwt <- baysc::wolca(x_mat = x_mat, sampling_wt = rep(1, nrow(x_mat)), 
+    res_unwt <- wolca(x_mat = x_mat, sampling_wt = rep(1, nrow(x_mat)), 
                              run_sampler = "both", K_max = K_max, 
                              adapt_seed = adapt_seed, class_cutoff = 0.05, 
                              n_runs = n_runs, burn = burn, thin = thin, 
@@ -112,93 +156,93 @@ if (already_done) {
 }
 
 
-#================== Local version ==============================================
-# Don't need to run if using cluster!
-# Run model locally for all samples
-num_samps <- 100
-for (samp_i in 1:num_samps) {
-  
-  # Define path to save results
-  save_path <- paste0(wd, res_dir, "scen_", scenario, "/samp_", samp_i)
-  
-  print(paste0('Running scenario ', scenario, ' samp ', samp_i, '...'))
-
-  # Source functions
-  source(paste0(wd, code_dir, "model_functions.R"))
-
-  ### Read in population and sample data
-  load(paste0(wd, data_dir, "scen_", scenario, "/sim_pop_wolcan.RData"))
-  load(paste0(wd, data_dir, "scen_", scenario, "/sim_samp_", samp_i, "_B_wolcan.RData"))
-  load(paste0(wd, data_dir, "scen_", scenario, "/sim_samp_", samp_i, "_R_wolcan.RData"))
-
-  # # Number of overlapping individuals
-  # length(intersect(sim_samp_R$ind_R, sim_samp_B$ind_B))
-
-  ### Define data and parameters
-  # Estimating pseudo-weights
-  x_mat <- sim_samp_B$X_data  # Multivariate categorical variables
-  dat_B <- data.frame(sim_samp_B$covs)  # Covariates for NPS
-  dat_R <- data.frame(sim_samp_R$covs)  # Covariates for reference
-  pred_covs_B <- c("A1", "A2", "A1A2")  # Covariates to predict NPS selection
-  pred_covs_R <- c("A1", "A2", "A1A2")  # Covariates to predict RS selection
-  pi_R <- sim_samp_R$pi_R  # RS selection probabilites for those in RS
-  hat_pi_R <- NULL  # RS selection probabilities for those in NPS
-  num_post <- 1000  # Number of posterior BART draws for estimating weights
-  frame_B <- 1  # Coverage probability of NPS frame
-  frame_R <- 1  # Coverage probability of RS frame
-
-  # Model estimation
-  D <- 10            # Number of sets of MI pseudo-weights
-  parallel <- TRUE   # Whether to parallelize for MI
-  MI <- TRUE         # Whether to run MI procedure for variance estimation
-  adjust <- TRUE     # Whether to adjust variance for pseudo-likelihood
-  tol <- 1e-8        # Underflow tolerance
-  run_adapt <- TRUE  # Whether to run adaptive sampler to get K
-  K_max <- 30          # Max number of latent classes for adaptive sampler
-  adapt_seed <- samp_i # Seed for adaptive sampler
-  fixed_seed <- samp_i # Seed for fixed sampler
-  n_runs <- 10000    # Number of MCMC iterations
-  burn <- 5000      # Burn-in
-  thin <- 5          # Thinning
-  update <- 1000     # Display update frequency
-  
-  # # For tests
-  # n_runs <- 1000
-  # burn <- 500
-  # thin <- 5
-  # update <- 500
-
-  ### Modifications based on scenario
-  if (scenario == 2) {  # No propagation of weights uncertainty
-    MI <- FALSE
-  }
-  if (scenario == 3) {  # No pseudo-likelihood adjustment
-    adjust = FALSE
-  }
-  if (scenario == 4) {  # RS weights known for NPS
-    hat_pi_R = sim_samp_B$true_pi_R
-  }
-
-  ### Run weighted model
-  res <- wolcan(x_mat = x_mat, dat_B = dat_B, dat_R = dat_R,
-                pred_covs_B = pred_covs_B, pred_covs_R = pred_covs_R,
-                pi_R = pi_R, hat_pi_R = hat_pi_R, num_post = num_post,
-                frame_B = frame_B, frame_R = frame_R, D = D, parallel = parallel,
-                MI = MI, adjust = adjust, tol = tol, run_adapt = run_adapt,
-                K_max = K_max, adapt_seed = adapt_seed, K_fixed = NULL,
-                fixed_seed = fixed_seed, class_cutoff = 0.05,
-                n_runs = n_runs, burn = burn, thin = thin, update = update,
-                save_res = TRUE, save_path = save_path)
-
-  ### Run unweighted model
-  if (scenario %in% c(1, 7:11)) {
-    res_unwt <- baysc::wolca(x_mat = x_mat, sampling_wt = rep(1, nrow(x_mat)),
-                             run_sampler = "both", K_max = K_max,
-                             adapt_seed = adapt_seed, class_cutoff = 0.05,
-                             n_runs = n_runs, burn = burn, thin = thin,
-                             update = update, save_res = TRUE,
-                             save_path = save_path)
-  }
-}
+# #================== Local version ==============================================
+# # Don't need to run if using cluster!
+# # Run model locally for all samples
+# num_samps <- 100
+# for (samp_i in 1:num_samps) {
+#   
+#   # Define path to save results
+#   save_path <- paste0(wd, res_dir, "scen_", scenario, "/samp_", samp_i)
+#   
+#   print(paste0('Running scenario ', scenario, ' samp ', samp_i, '...'))
+# 
+#   # Source functions
+#   source(paste0(wd, code_dir, "model_functions.R"))
+# 
+#   ### Read in population and sample data
+#   load(paste0(wd, data_dir, "scen_", scenario, "/sim_pop_wolcan.RData"))
+#   load(paste0(wd, data_dir, "scen_", scenario, "/sim_samp_", samp_i, "_B_wolcan.RData"))
+#   load(paste0(wd, data_dir, "scen_", scenario, "/sim_samp_", samp_i, "_R_wolcan.RData"))
+# 
+#   # # Number of overlapping individuals
+#   # length(intersect(sim_samp_R$ind_R, sim_samp_B$ind_B))
+# 
+#   ### Define data and parameters
+#   # Estimating pseudo-weights
+#   x_mat <- sim_samp_B$X_data  # Multivariate categorical variables
+#   dat_B <- data.frame(sim_samp_B$covs)  # Covariates for NPS
+#   dat_R <- data.frame(sim_samp_R$covs)  # Covariates for reference
+#   pred_covs_B <- c("A1", "A2", "A1A2")  # Covariates to predict NPS selection
+#   pred_covs_R <- c("A1", "A2", "A1A2")  # Covariates to predict RS selection
+#   pi_R <- sim_samp_R$pi_R  # RS selection probabilites for those in RS
+#   hat_pi_R <- NULL  # RS selection probabilities for those in NPS
+#   num_post <- 1000  # Number of posterior BART draws for estimating weights
+#   frame_B <- 1  # Coverage probability of NPS frame
+#   frame_R <- 1  # Coverage probability of RS frame
+# 
+#   # Model estimation
+#   D <- 10            # Number of sets of MI pseudo-weights
+#   parallel <- TRUE   # Whether to parallelize for MI
+#   MI <- TRUE         # Whether to run MI procedure for variance estimation
+#   adjust <- TRUE     # Whether to adjust variance for pseudo-likelihood
+#   tol <- 1e-8        # Underflow tolerance
+#   run_adapt <- TRUE  # Whether to run adaptive sampler to get K
+#   K_max <- 30          # Max number of latent classes for adaptive sampler
+#   adapt_seed <- samp_i # Seed for adaptive sampler
+#   fixed_seed <- samp_i # Seed for fixed sampler
+#   n_runs <- 10000    # Number of MCMC iterations
+#   burn <- 5000      # Burn-in
+#   thin <- 5          # Thinning
+#   update <- 1000     # Display update frequency
+#   
+#   # # For tests
+#   # n_runs <- 1000
+#   # burn <- 500
+#   # thin <- 5
+#   # update <- 500
+# 
+#   ### Modifications based on scenario
+#   if (scenario == 2) {  # No propagation of weights uncertainty
+#     MI <- FALSE
+#   }
+#   if (scenario == 3) {  # No pseudo-likelihood adjustment
+#     adjust = FALSE
+#   }
+#   if (scenario == 4) {  # RS weights known for NPS
+#     hat_pi_R = sim_samp_B$true_pi_R
+#   }
+# 
+#   ### Run weighted model
+#   res <- wolcan(x_mat = x_mat, dat_B = dat_B, dat_R = dat_R,
+#                 pred_covs_B = pred_covs_B, pred_covs_R = pred_covs_R,
+#                 pi_R = pi_R, hat_pi_R = hat_pi_R, num_post = num_post,
+#                 frame_B = frame_B, frame_R = frame_R, D = D, parallel = parallel,
+#                 MI = MI, adjust = adjust, tol = tol, run_adapt = run_adapt,
+#                 K_max = K_max, adapt_seed = adapt_seed, K_fixed = NULL,
+#                 fixed_seed = fixed_seed, class_cutoff = 0.05,
+#                 n_runs = n_runs, burn = burn, thin = thin, update = update,
+#                 save_res = TRUE, save_path = save_path)
+# 
+#   ### Run unweighted model
+#   if (scenario %in% c(1, 7:11)) {
+#     res_unwt <- wolca(x_mat = x_mat, sampling_wt = rep(1, nrow(x_mat)),
+#                              run_sampler = "both", K_max = K_max,
+#                              adapt_seed = adapt_seed, class_cutoff = 0.05,
+#                              n_runs = n_runs, burn = burn, thin = thin,
+#                              update = update, save_res = TRUE,
+#                              save_path = save_path)
+#   }
+# }
 
 
--- Simulation_Code/simulate_data.R
+++ Simulation_Code/simulate_data.R
@@ -8,8 +8,9 @@
 rm(list = ls())
 
 library(MASS)  # multivariate normal distribution
-library(baysc)  # swolca and data generation
+# library(baysc)  # swolca and data generation
 wd <- "~/Documents/GitHub/WOLCAN/"  # Working directory
+wd <- "/n/holyscratch01/stephenson_lab/Users/stephwu18/WOLCAN/"
 data_dir <- "Data/"                 # Data directory
 res_dir <- "Results/"               # Results directory
 code_dir <- "Simulation_Code/"      # Simulation code directory
@@ -17,7 +18,22 @@ code_dir <- "Simulation_Code/"      # Simulation code directory
 # Source simulation functions
 source(paste0(wd, code_dir, "simulate_data_functions.R"))
 
-#=================== Generate population =======================================
+# Source functions from baysc package
+file_list <- c("simulate_data.R")
+invisible(lapply(file_list, function(x) 
+  source(paste0(wd, "Model_Code/", "baysc_functions/", x))))
+
+#=================== Generate population: SCENARIO 1 ===========================
+# Variance adjustment, pi_R unknown and predicted using continuous BART, 
+# sample size 2000/40,000 (5%), non-overlapping patterns, X~S, 
+# high overlap for pi_R and pi_B
+scenario <- 1
+
+# Create data folder if it doesn't exist
+dir_path <- paste0(wd, data_dir, "scen_", scenario, "/")
+if (!dir.exists(dir_path)) {
+  dir.create(file.path(dir_path))
+}
 
 ### General parameters
 rho <- 0.5     # Correlation between selection variables A1 and A2
@@ -51,20 +67,7 @@ modal_theta_prob <- 0.85
 beta_list_x <- get_betas_x(profiles = profiles, R = R,
                            modal_theta_prob = modal_theta_prob,
                            formula_x = formula_x, V_unique = V_unique)
-# Add in coefficients for S to each j matrix, updating formula_x and beta_list_x
-beta_list_x_temp <- beta_list_x
-formula_x <- "~ c_all + A2"
-# Items 1-3 are affected in the following manner: 
-# level 4 probability increases as A2 increases
-beta_list_x <- lapply(1:3, function(j) cbind(beta_list_x_temp[[j]],
-                                             A2 = c(0, 0, 0, 5)))
-beta_list_x <- c(beta_list_x, lapply(4:J, function(j) cbind(beta_list_x_temp[[j]],
-                                                            A2 = rep(0, 4))))
-# beta_list_x <- lapply(1:3, function(j) cbind(beta_list_x_temp[[j]],
-#                                              s_all = c(0, 0.7, -0.7, -0.7)))
-# beta_list_x <- c(beta_list_x, lapply(4:J, function(j) cbind(beta_list_x_temp[[j]],
-#                                                             s_all = rep(0, 4))))
-# V_unique <- as.data.frame(expand.grid(c_all = as.factor(1:K), 
+# V_unique <- as.data.frame(expand.grid(c_all = as.factor(1:K),
 #                                       A2 = c(-8, -1.5, 0, 1.5, 8)))
 # get_categ_probs(beta_mat = beta_list_x[[1]], formula = formula_x, V_unique = V_unique)
 
@@ -73,9 +76,61 @@ sim_pop <- sim_pop_wolcan(N = N, J = J, K = K, R = R, rho = rho,
                       high_overlap = TRUE, formula_c = formula_c, 
                       beta_mat_c = beta_mat_c, formula_x = formula_x, 
                       beta_list_x = beta_list_x, pop_seed = pop_seed, 
-                      save_res = TRUE, save_path = paste0(wd, data_dir, "scen_1/")) 
+                      save_res = TRUE, save_path = dir_path) 
 
-### Check values for simulated data
+
+#=================== Generate population: SCENARIO 11 ==========================
+# X !~ S
+scenario <- 11
+
+# Create data folder if it doesn't exist
+dir_path <- paste0(wd, data_dir, "scen_", scenario, "/")
+if (!dir.exists(dir_path)) {
+  dir.create(file.path(dir_path))
+}
+
+### General parameters
+rho <- 0.5     # Correlation between selection variables A1 and A2
+N <- 40000     # Population size
+pop_seed <- 1  # Set seed
+
+### Parameters for generating categorical latent class assignment C
+formula_c <- "~ A1 + A2 + A1A2"
+beta_mat_c <- matrix(c(0, 0, 0, 0, 
+                       0.4, -0.5, 0.75, 0.1,  
+                       -0.2, -1, 1.2, 0.25), nrow = 3, byrow = TRUE)
+colnames(beta_mat_c) <- c("Intercept", "A1", "A2", "A1A2")
+
+### Parameters for generating observed manifest variables X
+J <- 30; R <- 4; K <- 3
+formula_x <- "~ c_all"
+V_unique <- data.frame(c_all = as.factor(1:K))
+profiles <- as.matrix(data.frame(C1 = c(rep(1, times = 0.5 * J),
+                                        rep(3, times = 0.5 * J)),
+                                 C2 = c(rep(4, times = 0.2 * J),
+                                        rep(2, times = 0.8 * J)),
+                                 C3 = c(rep(3, times = 0.3 * J),
+                                        rep(4, times = 0.4 * J),
+                                        rep(1, times = 0.3 * J))))
+modal_theta_prob <- 0.85
+beta_list_x <- get_betas_x(profiles = profiles, R = R,
+                           modal_theta_prob = modal_theta_prob,
+                           formula_x = formula_x, V_unique = V_unique)
+# V_unique <- as.data.frame(expand.grid(c_all = as.factor(1:K), 
+#                                       A2 = c(-8, -1.5, 0, 1.5, 8)))
+# get_categ_probs(beta_mat = beta_list_x[[1]], formula = formula_x, V_unique = V_unique)
+
+### Generate population
+n_B <- 2000  # Sample size for non-probability sample
+n_R <- 2000  # Sample size for reference sample
+sim_pop <- sim_pop_wolcan(N = N, J = J, K = K, R = R, rho = rho, n_B = n_B, 
+                          n_R = n_R, high_overlap = TRUE, formula_c = formula_c, 
+                          beta_mat_c = beta_mat_c, formula_x = formula_x, 
+                          beta_list_x = beta_list_x, pop_seed = pop_seed, 
+                          save_res = TRUE, save_path = dir_path) 
+
+
+#================= Check values for simulated population =======================
 # Selection probabilities and weights
 summary(sim_pop$pi_R)
 summary(sim_pop$pi_B)
@@ -83,18 +138,18 @@ summary(1 / sim_pop$pi_R)
 summary(1 / sim_pop$pi_B)
 hist(sim_pop$pi_R)
 hist(sim_pop$pi_B)
-cor(sim_pop$A1, sim_pop$pi_R)
-cor(sim_pop$A2, sim_pop$pi_R)
-cor(sim_pop$A1, sim_pop$pi_B)
-cor(sim_pop$A2, sim_pop$pi_B)
+cor(sim_pop$pop$A1, sim_pop$pi_R)
+cor(sim_pop$pop$A2, sim_pop$pi_R)
+cor(sim_pop$pop$A1, sim_pop$pi_B)
+cor(sim_pop$pop$A2, sim_pop$pi_B)
 
 # Latent class assignment correlations with selection covariates
-cor(sim_pop$c_all, sim_pop$A1)
-cor(sim_pop$c_all, sim_pop$A2)
+cor(as.numeric(sim_pop$c_all), sim_pop$pop$A1)
+cor(as.numeric(sim_pop$c_all), sim_pop$pop$A2)
 
 # Observed manifest variables correlations with selection covariates
-cor(sim_pop$X_data, sim_pop$A1)
-cor(sim_pop$X_data, sim_pop$A2)
+cor(sim_pop$X_data, sim_pop$pop$A1)
+cor(sim_pop$X_data, sim_pop$pop$A2)
 
 # Observed thetas
 # item 1 has association with A2
@@ -102,11 +157,13 @@ t(sapply(1:3, function(k) prop.table(table(sim_pop$X_data[sim_pop$c_all == k, 1]
 # item 4 does not have association with A2
 t(sapply(1:3, function(k) prop.table(table(sim_pop$X_data[sim_pop$c_all == k, 4]))))
 
+
+
 #================== Generate samples ===========================================
 ### Load population data 
-scenario <- 1
+scenario <- 11
 load(paste0(wd, data_dir, "scen_", scenario, "/sim_pop_wolcan.RData"))
-num_samps <- 10  # Number of samples
+num_samps <- 100  # Number of samples
 
 ### Parameters
 n_B <- 2000  # Sample size for non-probability sample
@@ -115,7 +172,7 @@ n_R <- 2000  # Sample size for reference sample
 for (i in 1:num_samps) {
   sim_samps <- sim_samp_wolcan(i = i, sim_pop = sim_pop, n_B = n_B, n_R = n_R, 
                                scenario = scenario, save_res = TRUE, samp_seed = i,
-                               save_path = paste0(wd, data_dir, "scen_1/"))
+                               save_path = dir_path)
 }
 
 
